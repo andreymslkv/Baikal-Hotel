3748e260f86c887d0c4bcb4a3ebc0086fe78d7af
Andrey Maslikov <andym33@yandex.ru>
2026-01-16 17:59:05 +0300

    set up the burger menu in the header

--- js/main.js
+++ js/main.js
@@ -65,3 +65,42 @@ if (confirmationDialog) {
   });
 }
 
+document
+  .querySelector(".header__burger-btn")
+  .addEventListener("click", function () {
+    const menu = document.querySelector(".menu");
+    const openIcon = document.querySelector(".icon-open");
+    const closeIcon = document.querySelector(".icon-close");
+
+    menu.classList.toggle("menu--active");
+
+    if (openIcon.classList.contains("hidden")) {
+      openIcon.classList.remove("hidden");
+      closeIcon.classList.add("hidden");
+    } else {
+      openIcon.classList.add("hidden");
+      closeIcon.classList.remove("hidden");
+    }
+  });
+
+// Обработка клика на пункты меню
+document.querySelectorAll(".menu__link").forEach((link) => {
+  link.addEventListener("click", function (e) {
+    // Удаляем класс активности со всех ссылок
+    document.querySelectorAll(".menu__link").forEach((l) => {
+      l.classList.remove("menu__link_active");
+    });
+
+    // Добавляем класс активности на кликнутую ссылку
+    this.classList.add("menu__link_active");
+
+    // Закрываем мобильное меню
+    const menu = document.querySelector(".menu");
+    const openIcon = document.querySelector(".icon-open");
+    const closeIcon = document.querySelector(".icon-close");
+
+    menu.classList.remove("menu--active");
+    openIcon.classList.remove("hidden");
+    closeIcon.classList.add("hidden");
+  });
+});
